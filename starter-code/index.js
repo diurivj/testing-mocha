efaa1d405d61f4840044b3bb26f05fd8bb131fbe
CGuillotG <c.guillot@outlook.com>
2019-02-04 17:50:21 -0600

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) =>{return a-b}) 
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos-1] === undefined) {
+      return "Error: OutOfBounds"
+    }
+    return this.items[pos-1]
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      return "Error: EmptySortedList";
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      return "Error: EmptySortedList";
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((a, b) => {
+      return a + b;
+    }, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.items.length;}
 }
 
 module.exports = SortedList
