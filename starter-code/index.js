c928b7548c0e875efa1727f722cbc05d2a89d9f3
javieramirez90 <javier.ramirez@aiesec.net>
2019-02-04 17:53:00 -0600

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    // if(this.items[pos - 1 ] == undefined){
+      this.length = this.items.length
+      if(pos > this.items.length){
+      return "Error: OutOfBounds"
+    } else {
+      return this.items[pos - 1];
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    let maxim = 0
+    if(this.items.length === 0){
+      return "Error: EmptySortedList"
+    } else {
+      this.items.forEach(x => {
+        if(x > maxim) maxim = x;
+      })
+    return maxim;
+  }
+}
 
-  sum() {}
+  min() {
+    if(this.items.length === 0){
+      return "Error: EmptySortedList"
+    } else {
+    return this.items.sort((a, b) => a - b)[0]
+    }
+  }
 
-  avg() {}
+  sum() {
+    let acum = 0
+    this.items.forEach(x => {
+      acum += x;
+    })
+    return acum;  
+  }
+  
+
+  avg() {
+    return this.sum()/this.items.length
+  }
 }
 
 module.exports = SortedList
