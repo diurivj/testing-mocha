e4af30a43f284f78a6562e44220819f845c85f21
iamalexrodriguez <alex.rdz.t@gmail.com>
2019-02-04 17:37:18 -0600

    ejercicio terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a,b) => (a-b));
+  }
+  get(pos) {
+    if(pos > this.items.length){
+      return "Error: OutOfBounds"
+    }
 
-  add(item) {}
+    return this.items[pos-1]
+  }
+  max() {
+    if(this.items.length === 0){
+      return new Error("EmptySortedList")
+    }
+    return Math.max.apply(null,this.items)
+  }
+  min() {
+    if(this.items.length === 0){
+      return new Error("EmptySortedList")
+    }
+    return Math.min.apply(null,this.items)
+  }
+  avg() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList")
+    }
 
-  get(pos) {}
+    let sum = this.items.reduce((previous,current)=> current += previous)
+    return sum / this.items.length
+  }
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }
 
-  max() {}
+    return this.items.reduce((previous, current) => current += previous)
 
-  min() {}
+  }
+};
 
-  sum() {}
+module.exports = SortedList;
 
-  avg() {}
-}
-
-module.exports = SortedList
