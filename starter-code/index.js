b8f4f3d81bfce176972468ec3c3a6f6caa4dffe5
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-02-04 18:51:59 -0500

    listo

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.length) return new Error("OutOfBounds");
+    return this.items[pos - 1];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      return new Error("EmptySortedList");
+    } else {
+      return this.items.sort((a, b) => b - a)[0];
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      return new Error("EmptySortedList");
+    } else {
+      return this.items.sort((a, b) => a - b)[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((a, b) => a + b, 0);
+    }
+  }
 
-  avg() {}
+  avg() {
+    return this.sum() / this.items.length;
+  }
 }
 
-module.exports = SortedList
+module.exports = SortedList;
