76b5644cb36c561f110db7b12e0e66886515e59c
Mario Mancilla <46686176+Legionario126@users.noreply.github.com>
2019-02-05 09:09:44 -0600

    Update index.js

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=>a-b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length != 0) {
+      return this.items[pos - 1];
+    }
+    else {
+      return new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length != 0) {
+      return Math.max(...this.items);
+    }
+    else {
+      return new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length != 0) {
+      return Math.min(...this.items);
+    }
+    else {
+      return new Error("EmptySortedList");
+    }
+  }
 
-  sum() {}
+  sum() {
+    let result = 0;
+    for (let i = 0; i < this.length; i++){
+      result += this.items[i];
+    }
+    return result;
+  }
 
-  avg() {}
+  avg() {
+      return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList
