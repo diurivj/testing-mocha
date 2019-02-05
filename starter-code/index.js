32af186a1ef474843e99c56fb4498952690d1120
Isabella Onate <isabellaonate@gmail.com>
2019-02-04 18:20:01 -0600

    Tests are not passing, trying to commit and test again

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,9 +1,21 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+      this.items = [];
+      this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a , b)=>{a < b})
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      return "Error: OutOfBounds"
+    } else {
+    return this.items[pos-1]
+    }
+  }
 
   max() {}
 
