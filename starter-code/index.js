746c473890c775038254804b08aca9a2e14cc844
icarlos1 <ruizdominguezcarlos@icloud.com>
2019-02-05 15:07:24 -0600

    terminado

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
-}
-
-module.exports = SortedList
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+ 
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length++;
+  }
+ 
+  get(pos) {
+    if (this.items[pos - 1] === undefined) {
+      return new Error("OutOfBounds");
+    }
+    return this.items[pos - 1];
+  }
+ 
+  max() {
+    if (this.length === 0) {
+      return new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+ 
+  min() {
+    if (this.length === 0) {
+      return new Error("EmptySortedList");
+    } else {
+      return this.items.sort((a, b) => a - b)[0];
+    }
+  }
+ 
+  sum() {
+    let summ = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      summ += this.items[i];
+    }
+    return summ;
+  }
+ 
+  avg() {
+    if (this.length === 0) {
+      return new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, current) => acc + current) / this.length;
+    }
+  }
+ }
+ 
+ module.exports = SortedList;
