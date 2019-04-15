5a008386947e36fff0ea86e8efa969be662670c1
pipepico <fpr29.10@gmail.com>
2019-04-15 12:29:11 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,49 @@
+// PP MEX Albert Felipe
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    
+    return this.length
+  }
 
-  add(item) {}
+  get(pos) {
+    if (this.items.indexOf(pos) !== -1) {
+      return pos
+    } else {
+      return 'Error: OutOfBounds'
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if (this.length == 0) {
+    return new Error("EmptySortedList")
+    } else{
+      console.log(Math.max(...this.items));
+      return Math.max(...this.items)
+    }
 
-  max() {}
+  }
 
-  min() {}
+  min() {
+    if (this.length == 0) {
+      return new Error("EmptySortedList")
+      } else{
+        return Math.min(...this.items)
+      }
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((a, b) => a + b,0)
+  }
 
-  avg() {}
+  avg() {
+    return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList
