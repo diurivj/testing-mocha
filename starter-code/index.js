014786d3bd181724077f5b450a780c21bee569f9
gustavvopenna <gustavo.pema@gmail.com>
2019-04-15 13:09:23 -0500

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(x) {
+    this.items.push(x)
+    this.length += 1
+  }
 
-  add(item) {}
+  get(pos){
+    if (this.items[pos - 1] === undefined) {
+      return new Error('OutOfBounds')
+    }else {
+      return this.items[pos - 1]
+    }
+  }
 
-  get(pos) {}
+  max(){
+    //var max = Math.max(...arr);
+    if (this.length === 0) {
+      return new Error('EmptySortedList')
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  max() {}
+  min(){
+    if (this.length === 0) {
+      return new Error('EmptySortedList')
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  min() {}
+  sum(){
+    if (this.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce((a, b) => a + b, 0)
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  avg(){
+    return this.items.reduce((a, b) => a + b, 0) / this.length
+  }
 }
+
+let list = new SortedList()
+list.add(0)
+list.get(1)
 
 module.exports = SortedList
