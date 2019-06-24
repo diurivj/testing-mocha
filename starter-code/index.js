0d39659c4d077ae446f0bf2a82dfa2931f3f8626
oscarmtzo <icehman@hotmail.com>
2019-06-24 13:38:10 -0500

    Done PP-FT-MX Jorge Martinez y Oscar MArtinez

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    
+    this.length = this.items.push(item)
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos -1]){
+      return this.items[pos -1]
+    }else {return 'Error: OutOfBounds'}
+  }
 
-  max() {}
+  max() {
+    let max =  Math.max(...this.items)
+    if (this.length === 0){
+      return 'Error: EmptySortedList'
+    }else {
+      return max
+    }
+  
+  }
 
-  min() {}
+  min() {
+    let min = Math.min(...this.items)
+    if (this.length ===0){
+      return 'Error: EmptySortedList'
+    }else{
+      return min
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = this.items.reduce((a,b)=>a+b,0)
+    if (this.length ===0){
+      return 0
+    } else {
+      return sum
+    }
+  }
 
-  avg() {}
+  avg() {
+    let avg = this.items.reduce((a,b)=>a+b,0)/this.length
+    return avg
+  }
 }
 
 module.exports = SortedList
