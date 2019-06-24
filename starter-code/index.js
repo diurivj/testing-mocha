d64670870408c2b65750a73683b5530fe04f1696
bichous <bichous07@gmail.com>
2019-06-24 13:32:17 -0500

    Done testing

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a-b)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    console.log(this.items)
+    console.log(pos)
+    if (this.items[pos - 1] === undefined) {
+      return 'Error: OutOfBounds'
+      
+    } else {
+      return this.items[pos - 1]
+      
+    }
 
-  max() {}
+  } 
 
-  min() {}
+  max() {
+if (this.items.length === 0) {
+  return 'Error: EmptySortedList'
+}else{
+  this.items.sort((a,b) => b-a)
+  return this.items[0]
+}
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      return 'Error: EmptySortedList'
+    }else{
+      this.items.sort((a,b) => a-b)
+      return this.items[0]
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    }else{
+    return  this.items.reduce((a,b) => a+b)
+    }
+  }
+
+  avg() {
+    return  this.items.reduce((a,b) => a+b)/this.items.length
+  }
 }
 
 module.exports = SortedList
