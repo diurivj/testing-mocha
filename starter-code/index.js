a4d23d648d393f2a02a425241e96bad08f41738c
Daniel Orio <danielislas3@live.com>
2019-06-24 13:13:36 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+      this.length = this.items.length
 
-  add(item) {}
 
-  get(pos) {}
+  }
 
-  max() {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a, b) => a - b)
+    this.length++
+  }
 
-  min() {}
+  get(pos) {
+    if (this.items[pos - 1] === undefined) {
+      return 'Error: OutOfBounds'
+    } else {
+      return this.items[pos - 1]
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  max() {
+
+
+    if (this.items.length === 0) {
+      return 'Error: EmptySortedList'
+    } else {
+      this.items.sort((a, b) => b - a)
+      return this.items[0]
+    }
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      return 'Error: EmptySortedList'
+    } else {
+      this.items.sort((a, b) => a - b)
+      return this.items[0]
+    }
+
+  }
+
+  sum() {
+    if (this.items.length === 0) return 0
+    return this.items.reduce((pr, cu) => pr + cu)
+  }
+  avg() {
+    return this.items.reduce((pr, cu) => pr + cu) / this.items.length
+  }
 }
 
 module.exports = SortedList
