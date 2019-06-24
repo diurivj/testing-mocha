c3b29ee67e44842533ef0a16b331c6db9190e0e0
mariana-gamino <mariianagamino@gmail.com>
2019-06-24 13:16:08 -0500

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,71 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item)
+      this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    console.log(this.items)
+    console.log(this.length)
+    console.log(this)
+    if(pos <= this.length) {
+      return this.items[pos-1]} else {
+        return new Error('OutOfBounds')
+      }
+  }
 
-  max() {}
+  max() {
+    let max = 0
+    if(this.length > 0){
+      this.items.forEach(item => {
+      if(item > max) max = item
+    })
+    return max
+    } else {
+      return new Error("EmptySortedList")
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length > 0){
+      let min = this.items[0]
+      this.items.forEach(item => {
+        if(item < min) min = item
+      })
+      return min
+      } else {
+        return new Error("EmptySortedList")
+      }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length > 0){
+      let sum = 0
+      this.items.forEach(item => {
+        sum+=item
+      })
+      return sum
+      } else {
+        return 0
+      }
+  }
 
-  avg() {}
+  avg() {
+    if(this.length > 0){
+      let sum = 0
+      this.items.forEach(item => {
+        sum+=item
+      })
+      return sum/this.length
+      } else {
+        return 0
+      }
+  }
 }
 
 module.exports = SortedList
