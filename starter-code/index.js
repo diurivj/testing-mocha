7effe35d7a3936d4c39db6dd57ad7d799090ee4e
jose1987barajas <jose.1987.barajas@gmail.com>
2019-06-24 13:55:49 -0500

    PP-FT-MX

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length    
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length   
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos-1 >= this.items.length || pos-1 < 0) return 'Error: OutOfBounds';
+    return this.items[pos -1]
+    
+  }
+  
 
-  min() {}
+  max() {
+    if (this.items.length === 0) return 'Error: EmptySortedList';
+    return this.items.sort()[this.items.length-1];
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) return 'Error: EmptySortedList';
+    return this.items.sort()[0];
+  }
 
-  avg() {}
+  sum() {
+    if (this.items.length === 0) return 0;
+    let counter = 0;
+    this.items.forEach( (el) => {
+      counter += el
+    })
+    return counter;
+  }
+
+  avg() {
+    if (this.items.length === 0) return 0;
+    return this.sum() / this.items.length
+  }
 }
 
 module.exports = SortedList
