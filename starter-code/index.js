e83d518cf522cd1107f0fac80585dd36e5fa7319
viveralia <edgarv10@gmail.com>
2019-09-02 13:32:50 -0500

    Done 🤙

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length += 1
+  }
 
-  get(pos) {}
+  get(pos) {
+    const result = this.items[pos - 1]
+    if (result) {
+      return result
+    } else {
+      return 'Error: OutOfBounds'
+    }
+  }
 
-  max() {}
+  max() {
+    let maxVal = 0
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] > maxVal) {
+        maxVal = this.items[i]
+      }
+    }
+    if (this.items.length === 0) {
+      return 'Error: EmptySortedList'
+    } else {
+      return maxVal
+    }
+  }
 
-  min() {}
+  min() {
+    let minVal = Number.POSITIVE_INFINITY
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] < minVal) {
+        minVal = this.items[i]
+      }
+    }
+    if (this.items.length === 0) {
+      return 'Error: EmptySortedList'
+    } else {
+      return minVal
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce((acc, cv) => acc + cv)
+    }
+  }
 
-  avg() {}
+  avg() {
+    return this.sum() / this.items.length
+  }
 }
 
 module.exports = SortedList
