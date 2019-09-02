ed0170a366bbfa59f6af69feea433f92309afe3d
erivasf <elias_rf@icloud.com>
2019-09-02 13:49:40 -0500

    Yastuvo

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+  }
+  get(pos) {
+    if (pos > this.items.length) {
+      return ('Error: OutOfBounds')
+    } else {
+      return this.items[pos - 1]
+    }
+  }
+  max() {
+    if (this.items.length === 0)
+      return 'Error: EmptySortedList'
+    else {
+      this.items.sort()
+      return this.items[this.items.length - 1]
+    }
+  }
 
-  add(item) {}
+  min() {
+    if (this.items.length === 0)
+      return 'Error: EmptySortedList'
+    else {
+      this.items.sort()
+      return this.items[0]
+    }
+  }
+  sum() {
+    if (this.items.length === 0)
+      return 0
+    else
+      return this.items.reduce((suma, current) => suma += current)
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    return this.sum() / this.items.length
+  }
 }
 
 module.exports = SortedList
