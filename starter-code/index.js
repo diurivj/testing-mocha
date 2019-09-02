7e844ff186ecf7041427dd44bdfb3019b2ed6250
Tania Marin <nicte.tania@gmail.com>
2019-09-02 14:05:22 -0500

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos - 1]) {
+      return new Error('OutOfBounds')
+    } else {
+      return this.items[pos-1]
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      return new Error('EmptySortedList')
+    }
+    else {
+    return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      return new Error('EmptySortedList')
+    } else {
+    return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    if (this.length!==0){
+      return this.items.reduce((a,b) => a+b)
+    }
+    else {
+      return 0
+    }
+  }
 
-  avg() {}
+  avg() {
+    return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList
