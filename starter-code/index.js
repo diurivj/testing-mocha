5ddba5f158ba5fd61b17f2b80bf0dc05054566e5
OttoCodeBerlin <ottoschmidberlin@gmail.com>
2019-09-02 13:47:42 -0500

    Done :hankey:

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items[pos - 1]) {
+      return 'Error: OutOfBounds'
+    }
+    else {
+      return this.items[pos - 1]
+    }
+  }
 
-  max() {}
+  max() {
 
-  min() {}
+    if (this.items.length === 0) {
+      return new Error("EmptySortedList")
+    } else {
+      return Math.max(...this.items)
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      return new Error("EmptySortedList")
+    } else {
+      return Math.min(...this.items)
+    }
+  }
+
+  sum() {
+
+    if (this.items.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce((a, b) => a + b, 0)
+    }
+
+
+  }
+
+  avg() {
+    return (this.items.reduce((a, b) => a + b, 0)) / this.items.length
+
+  }
 }
 
 module.exports = SortedList
