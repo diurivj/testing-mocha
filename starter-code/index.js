d8cfdbef47f92ffc294358dce368bdf10a3e6c02
EDGAR ALFREDO CHAVERO MOYA <ediechavero@Edie.local>
2019-09-02 16:10:57 -0500

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+      this.items.sort(function(a,b){return a>b ? 1: a< b ? -1 : 0})
+      this.length = this.items.length
 
-  get(pos) {}
+}
 
-  max() {}
+  get(pos) {
+    if (pos > this.items.length ) {
+      return new Error("Error: OutOfBounds");
+    } else {
+      return this.items[pos -1]
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      return new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length -1]
+    }
 
-  sum() {}
+    
 
-  avg() {}
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      return new Error("EmptySortedList");
+    } else {
+      return this.items[0]
+    }
+  }
+
+  sum() {
+    if(this.items.length ===0) {
+      return 0
+    }else {
+      return this.items.reduce((a,b)=> a+b)
+    }
+  }
+
+  avg() {
+    if(this.item === 0) {
+      return new Error("EmptySortedList")
+    }else{
+      return this.sum() / this.items.length
+    }
+  }
 }
 
 module.exports = SortedList
