6272b2eec18917d5ed0b6fabaefe680413055dc8
jcazorla2000 <jose.cazorla00@gmail.com>
2019-11-11 16:00:35 -0600

    PP-WDFT-MEX Diana Cruz & Jose Manuel Cazorla

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,27 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+    this.length = 0
+    this.items = []
+  }
+  add(item) {
+    this.items.push(item)
+    this.length++
+  }
+  get(pos) {
+    return (this.items[pos-1]) ? this.items[pos-1] : ("Error: OutOfBounds")
+  }
+  max() {
+    return (this.items.sort()[this.items.length - 1]) ? this.items.sort()[this.items.length - 1] : ("Error: EmptySortedList") 
+  }
+  min() {
+    return (this.items.sort()[this.items.length - 1]) ? this.items.sort()[0] : ("Error: EmptySortedList")
+  }
+  sum() {
+    return (this.items[0]) ? this.items.reduce((acc, element)=> acc += element) : 0
+  }
+  avg() {
+    return this.sum() / this.items.length
+  }
 }
 
 module.exports = SortedList
