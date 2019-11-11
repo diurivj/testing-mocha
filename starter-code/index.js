56b85b90efaf582cb6556e32579339293816fdbf
EChColorado <edxz7c@gmail.com>
2019-11-11 16:37:28 -0600

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a,b)=>{return a - b;})
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.items.length) return new Error("OutOfBounds");
+    return this.items[this.items.length-1]
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0) return new Error("EmptySortedList");
+    return this.items[this.items.length-1];
 
-  min() {}
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length === 0) return new Error("EmptySortedList");
+    return this.items[0];
+  }
 
-  avg() {}
+  sum() {
+    if(this.items.length === 0) return 0
+    return this.items.reduce((acc, el) => acc + el);
+  }
+
+  avg() {
+      if(this.length === 0) return new Error("EmptySortedList");
+      return this.sum()/(this.items.length);
+  }
 }
 
 module.exports = SortedList
+
+
+// let sortedlist = new SortedList();
+// console.log(sortedlist.length)
+// sortedlist.add(0)
+// console.log(sortedlist.length)
