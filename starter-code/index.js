d23f53ccea83581512390ef5d3563f643cb12398
RicardoSalas17 <ricardosalas.1716@gmail.com>
2019-11-11 17:30:36 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+     this.length = this.items.length ;
+ 
+  }
 
-  add(item) {}
+  add(x) {
+    this.items.push(x);
+    this.length = this.items.length ;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < this.items.length + 1){
+    return this.items[pos - 1]
+    
+    }else {
+      return 'Error: OutOfBounds'
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length !== 0 ){
+      return Math.max(...this.items)
+    } else {
+      return "Error: EmptySortedList"
+    }
+  }
 
-  min() {}
+  min() {
 
-  sum() {}
+    if(this.items.length !== 0){
+      return Math.min(...this.items)
+    } else {
+      return "Error: EmptySortedList"
+    }
+  }
+  
 
-  avg() {}
+  sum() {
+    if(this.items.length !== 0){
+      return this.items.reduce((acum, num) => acum + num, 0)
+    } else {
+      return 0
+    }
+
+  }
+
+  avg() {
+    if(this.items.length !== 0){
+      return this.items.reduce((acum, num) => acum + num, 0) / this.items.length
+    } else {
+      return 0
+    }
+  }
 }
 
 module.exports = SortedList
