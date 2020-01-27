c507202a9a8d6167f298bb965c3f1272b75b618b
Luis Lozano <luislozano@MacBook-Air.local>
2020-01-27 16:26:41 -0600

    Done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    //let items 
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => a-b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if (pos > this.length || pos < 0) {
+      return new Error("OutOfBounds")
+    } else {
+      return this.items[pos-1]
+    }
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if (this.length <= 0) {
+      return new Error('EmptySortedList')
+    } else {
+      return this.items[this.length - 1]
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.length <= 0) {
+      return new Error('EmptySortedList')
+    } else {
+      return this.items[0]
+    }
+  }
+
+  sum() {
+    if (this.items.length == 0 ) {
+      return 0
+    } else {
+      return this.items.reduce((accumulator, currentValue) => accumulator + currentValue)
+    }
+  }
+
+  avg() {
+    return this.sum() / this.length
+  }
 }
 
 module.exports = SortedList
