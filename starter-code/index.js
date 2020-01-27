9fa81e67e9bdcdb18cfb1d1e79399e6f236e8f84
Karlish Vázquez <karlishvazquez@Karlishs-MacBook-Pro-748.local>
2020-01-27 16:06:32 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items.sort()
+  }
+  get(pos) {
+    if(pos > this.length)
+      return new Error("OutOfBounds")
+      return this.items[pos -1]
+    }
+  max() {
+    if(this.items.length === 0) {
+      return new Error("EmptySortedList")
+    } else {
+    return Math.max(...this.items);
+    }
+  } 
+  min() {
+    if(this.items.length === 0) {
+      return new Error("EmptySortedList")
+    } else {
+    return Math.min(...this.items);
+    }
+  }
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    } else {
+      return this.items.reduce((acc,b) => acc += b)
+    }
+  }
+  avg() {
+    return this.sum()/this.length
+  }
 }
 
 module.exports = SortedList
