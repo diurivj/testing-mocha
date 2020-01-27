e37a4753db2fe6913db06546219d6d2c8ddf4c59
Ale Ochoa <ale.ochoafi@gmail.com>
2020-01-27 15:37:05 -0600

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[],
+    this.length=this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort()
+    this.length=this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.length<pos) {
+      return new Error("OutOfBounds")
+    }else{
+      return this.items[pos-1]
+    }
 
-  max() {}
+    
+  }
 
-  min() {}
+  max() {
+    if (this.length===0){
+      return new Error("EmptySortedList")
+    }else{
+      return this.items[this.length-1]
+    }
 
-  sum() {}
+  }
 
-  avg() {}
+  min() {
+    if (this.length===0){
+      return new Error("EmptySortedList")
+    }else{
+      return this.items[0]
+    }
+  }
+
+  sum() {
+    return this.items.reduce((acc,c)=>acc+c,0)
+  }
+
+  avg() {
+    if (this.length===0){
+      return new Error("EmptySortedList")
+    }else{
+      return this.sum()/this.length
+    }
+  }
 }
 
 module.exports = SortedList
